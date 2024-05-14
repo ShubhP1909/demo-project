9e9c0738be0fa2aa51b392e630920cb98684ed45
ShubhP1909 <shubhamprajapati1909@gmail.com>
2024-05-15 03:46:52 +0530

    init commit changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { MainBannerComponent } from './main-banner/main-banner.component';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { WelcomeBannerComponent } from './welcome-banner/welcome-banner.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MainBannerComponent, HeroSectionComponent, WelcomeBannerComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
